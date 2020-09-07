b2b43df37d4de14507558724b73bb94d38092e0e
Suvaline Vana <suvaline.vana@gmail.com>
2020-09-07 15:49:37 +0300

    first commit

---
+++ src/components/Test.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="text-center">
+    <v-menu offset-y>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn 
+          color="primary"
+          dark
+          elevation
+          v-bind="attrs"
+          v-on="on"
+        >
+          Dropdown
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item
+          v-for="(item, index) in items"
+          :key="index"
+          @click="greet"
+        >
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Test',
+
+    data: () => ({
+      items: [
+        { title: 'Click Me' },
+        { title: 'Click Me' },
+        { title: 'Click Me' },
+        { title: 'Click Me 2' },
+      ],
+    }),
+    methods: {
+        greet: function() {
+            alert("hello")
+        }
+    },
+  }
+</script>
